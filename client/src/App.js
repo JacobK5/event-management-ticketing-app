25846b26a4b5a94b9f2892732d50f8623a63253e
JacobK5 <jacobkoep@gmail.com>
2024-11-26 20:15:47 -0700

    Added example component
    Added component that shows off key react concepts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Counter from "./pages/Example";
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <Counter />
     </>
   );
 };
