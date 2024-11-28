0709dee916c48c569f7c6078d98b81c07b6c55e9
kev9268 <kevinhoang9268@gmail.com>
2024-11-27 17:35:39 -0700

    Organizer home page

---
+++ client/src/components/Organizer_Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Organizer_Header = () => {
+  return (
+    <header>
+      <nav className="menu">
+        <div className="menu-left">
+          <a href="#" className="menu-item">
+            Concerts
+          </a>
+          <a href="#" className="menu-item">
+            Sports
+          </a>
+          <a href="#" className="menu-item">
+            Theatre
+          </a>
+          <a href="#" className="menu-item">
+            Family
+          </a>
+        </div>
+        <div className="menu-right">
+          <a href="#" className="menu-item sign-in">
+            👤 Sign In
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Organizer_Header;
---
+++ client/src/pages/Organizer_Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Organizer_Home = () => {
+  return (
+    <>
+      <Header/>
+      <main className="center-content">
+        {/* create new event */}
+        {/* create new discount */}
+      
+      </main>
+    </>
+    
+    
+  );
+};
+
+export default Organizer_Home;
